09906e332c74cf61fcf1cd13515185413e35b463
ZhengLinLei <zheng9112003@gmail.com>
2023-12-13 09:49:11 +0100

    Redo Day13

--- 2023/Day13/python/Day13.py
+++ 2023/Day13/python/Day13.py
@@ -1,10 +1,6 @@
 # Part 1 and Part 2
 # Zheng Lin Lei
 block = [p.splitlines() for p in open('../in').read().split("\n\n")]
-
-def df(x, i): return sum(sum(a != b for a, b in zip(l[i:], l[i - 1 :: -1])) for l in x)
-def m(x, y): return sum(j for j in range(1, len(x[0])) if df(x, j) == y)
-def s(x, y): return m(x, y) + 100 * m([*zip(*x)], y)
-
-print("Part 1: ", sum(s(p, 0) for p in block))
-print("Part 2: ", sum(s(p, 1) for p in block))
+def s(x, y): m = lambda x, y: sum(j for j in range(1, len(x[0])) if sum(sum(a != b for a, b in zip(l[j:], l[j - 1 :: -1])) for l in x) == y); return m(x, y) + 100 * m([*zip(*x)], y)
+print("Part 1: ", sum(s(i, 0) for i in block))
+print("Part 2: ", sum(s(i, 1) for i in block))
